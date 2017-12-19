830165048c89167bdfbcb69beee6aeefa69c2bfe
aruvee <aruvee@gmail.com>
2017-12-19 20:52:33 +0530

    BSE added

--- FOStocks.py
+++ FOStocks.py
@@ -14,6 +14,52 @@ class FOStocks:
         dataframe=dataframe.loc[dataframe['EXP_DATE'] == '28-12-17']
         return dataframe
 
+    def getbsepandas(self, path):
+        dataframe= pandas.read_csv(path, index_col=1)
+        return dataframe
+
+    def getbsetodaypath(self):
+        prefix = "data\\EQ"
+        suffix = ".csv"
+        today = datetime.now()
+        year = today.year - 2000
+        month = today.month
+        if month < 10:
+            month = "0" + str(month)
+        day = today.day
+        if day < 10:
+            day = "0" + str(day)
+        filepath = prefix + str(day) + str(month) + str(year) + suffix
+        return filepath
+
+    def getbseprevpath(self):
+        prefix = "data\\EQ"
+        suffix = ".csv"
+        today = datetime.now() - timedelta(days=1)
+        year = today.year - 2000
+        month = today.month
+        if month < 10:
+            month = "0" + str(month)
+        day = today.day
+        if day < 10:
+            day = "0" + str(day)
+        filepath = prefix + str(day) + str(month) + str(year) + suffix
+        return filepath
+
+    def getbsepprevpath(self):
+        prefix = "data\\EQ"
+        suffix = ".csv"
+        today = datetime.now() - timedelta(days=2)
+        year = today.year - 2000
+        month = today.month
+        if month < 10:
+            month = "0" + str(month)
+        day = today.day
+        if day < 10:
+            day = "0" + str(day)
+        filepath = prefix + str(day) + str(month) + str(year) + suffix
+        return filepath
+
     def gettodaypath(self):
         prefix = "data\\fo"
         suffix = ".csv"
